76ca55a1d3d1f0c8c158cdc5c2f5aefedd772944
Gino <sahagun.gino@gmail.com>
2017-11-07 23:04:09 -0800

    made database and added section table

--- Memberships/Models/IdentityModels.cs
+++ Memberships/Models/IdentityModels.cs
@@ -3,6 +3,7 @@ using System.Security.Claims;
 using System.Threading.Tasks;
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
+using Memberships.Entities;
 
 namespace Memberships.Models
 {
@@ -29,5 +30,8 @@ namespace Memberships.Models
         {
             return new ApplicationDbContext();
         }
+
+        public DbSet<Section> Sections { get; set; }
+
     }
 }
--- Memberships/Scripts/AdminMenu.js
+++ Memberships/Scripts/AdminMenu.js
@@ -1,7 +1,14 @@
 ﻿$(function () {
+    //Admin Menu is Opened Through Clicking 
     var adminMenu = $('[data-admin-menu]');
-    adminMenu.hover(function () {
+    adminMenu.click((event) => {
+        event.stopPropagation();
         adminMenu.toggleClass('open');
     });
-    
+    //Admin Menu is Opened User Clicks on Document and closes menu
+    $(document).click(() => {
+        if (adminMenu.hasClass('open'))
+            adminMenu.removeClass('open');
+    });
+
 });
