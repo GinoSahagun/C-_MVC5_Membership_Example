a22f3907fce6dc77917b7d902ba8e1ebf70df6fb
Gino <sahagun.gino@gmail.com>
2017-11-15 18:25:02 -0800

    Section CRUD Operations Working

--- Memberships/Models/IdentityModels.cs
+++ Memberships/Models/IdentityModels.cs
@@ -4,12 +4,17 @@ using System.Threading.Tasks;
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
 using Memberships.Entities;
+using System;
 
 namespace Memberships.Models
 {
     // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit http://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
     public class ApplicationUser : IdentityUser
     {
+        
+        public string FirstName { get; set; }
+        public bool isActive { get; set; }
+        public DateTime? Registered { get; set; }
         public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
         {
             // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
