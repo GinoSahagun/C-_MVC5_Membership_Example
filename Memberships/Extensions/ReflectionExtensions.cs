7e3370baac7d2606ddf187c471057f8c9a40d48c
Gino <sahagun.gino@gmail.com>
2017-11-17 15:26:30 -0800

    added GetPropertyValue in Reflections Class and ToSelectListItem in ICollectionExtension Class

---
+++ Memberships/Extensions/ReflectionExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Memberships.Extensions
+{
+    public static class ReflectionExtensions
+    {
+        public static string GetPropertyValue<T>(this T item, string PropertyName)
+        {
+            return item.GetType()
+                .GetProperty(PropertyName)
+                .GetValue(item,null).ToString();
+        }
+    }
+}
