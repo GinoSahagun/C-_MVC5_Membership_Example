73c77ee088c76243c18f40e6009fb7787f375453
Gino <sahagun.gino@gmail.com>
2017-11-17 21:24:26 -0800

    ConversionExtensions Class + Convert Function

---
+++ Memberships/Extensions/ConversionExtentions.cs
@@ -0,0 +1,37 @@
+﻿using Memberships.Areas.Admin.Models;
+using Memberships.Entities;
+using Memberships.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace Memberships.Extensions
+{
+    public static class ConversionExtentions
+    {
+        public static async Task<IEnumerable<ProductModel>>  Convert(
+            this IEnumerable<Product> products,
+            ApplicationDbContext db )
+        {
+            var texts = await db.ProductLinkTexts.ToListAsync();
+            var types = await db.ProductTypes.ToListAsync();
+
+            return from p in products
+                   select new ProductModel
+                   {
+                       Id = p.Id,
+                       Title = p.Title,
+                       Description = p.Description,
+                       ImageUrl = p.ImageUrl,
+                       ProductLinkTextId = p.ProductLinkTextId,
+                       ProductTypeId = p.ProductTypeId,
+                       ProductLinkTexts = texts,
+                       ProductTypes = types
+                   };
+
+        }
+    }
+}
