c7eeed892f0f41342d6da932dd2f1f655b0bd9fb
Gino <sahagun.gino@gmail.com>
2017-11-22 18:04:17 -0800

    User Subscription Table Added + Remove Button Working to Remove Subscriptions

--- Memberships/Controllers/AccountController.cs
+++ Memberships/Controllers/AccountController.cs
@@ -734,6 +734,30 @@ namespace Memberships.Controllers
             return RedirectToAction("Subscriptions", "Account", new { userId = model.UserId });
         }
 
+        [Authorize(Roles = "Admin")]
+        public async Task<ActionResult> RemoveUserSubscriptions(string userId, int subscriptionId)
+        {
+            try
+            {
+                if (userId == null || userId.Equals(String.Empty) || subscriptionId <= 0)
+                {
+                    return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+                }
+                if (ModelState.IsValid)
+                {
+
+                    var db = new ApplicationDbContext();
+                    var subscriptions = db.UserSubscriptions.Where(us => us.UserId.Equals(userId) && us.SubscriptionId.Equals(subscriptionId));
+                    db.UserSubscriptions.RemoveRange(subscriptions);
+
+                    await db.SaveChangesAsync();
+
+                }
+            }
+            catch { }
+            return RedirectToAction("Subscriptions", "Account", new { userId = userId });
+        }
+
 
 
     }
