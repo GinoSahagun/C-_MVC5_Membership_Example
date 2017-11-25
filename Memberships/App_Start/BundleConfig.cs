ad94f554228e8e6274c094bef1fc55c06dcbbacf
Gino <sahagun.gino@gmail.com>
2017-11-25 15:03:31 -0800

    Register User Model + Password Recovery + TODO REFACTOR LATER

--- Memberships/App_Start/BundleConfig.cs
+++ Memberships/App_Start/BundleConfig.cs
@@ -29,6 +29,8 @@ namespace Memberships
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
+            bundles.Add(new StyleBundle("~/Content/membership").Include(
+                "~/Content/navbar.css"));
         }
     }
 }
